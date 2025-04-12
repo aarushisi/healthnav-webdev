53f2b225f16dd7b44e6961109f1366a55209b297
Aniket Dixit <anidixit@umich.edu>
2025-04-12 12:45:19 -0400

    Updated database and healthnav.db schema

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 import sqlite3
 import pandas as pd
+import os
+
 
 # Database file path
 DB_FILE = "healthnav.db"
-CSV_FILE = "cleaned_dataset.csv"  # Ensure this CSV is in the same directory
+CSV_FILE = "MI_Doctors.csv"  # Ensure this CSV is in the same directory
 
 def create_database():
+    if os.path.exists(DB_FILE):
+        os.remove(DB_FILE)
+        
     """Creates the SQLite database and required tables."""
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
@@ -29,6 +34,8 @@ def create_database():
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS doctors (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+            row_id INTEGER,
+            npi TEXT,
             last_name TEXT,
             first_name TEXT,
             degree TEXT,
@@ -39,7 +46,8 @@ def create_database():
             zip_code TEXT,
             country TEXT,
             phone TEXT,
-            gender TEXT
+            gender TEXT,
+            specialty TEXT
         )
     ''')
 
@@ -47,28 +55,23 @@ def create_database():
     conn.close()
 
 def load_doctors_data():
-    """Loads data from `cleaned_dataset.csv` into the doctors table."""
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
 
-    # Read CSV file
     df = pd.read_csv(CSV_FILE, dtype={"zip_code": str}, low_memory=False)
+    df.rename(columns={"Unnamed: 0": "row_id", "NPI": "npi"}, inplace=True)
 
-
-    # Ensure column names match the database schema
     df.columns = [col.lower().replace(" ", "_") for col in df.columns]
 
-    # Convert zip codes and phone numbers to strings (avoid scientific notation issues)
     df['zip_code'] = df['zip_code'].astype(str)
     df['phone'] = df['phone'].astype(str)
 
-    # Insert into SQLite table
     df.to_sql("doctors", conn, if_exists="replace", index=False)
 
     conn.commit()
     conn.close()
 
 if __name__ == "__main__":
-    create_database()  # Step 1: Create tables
-    load_doctors_data()  # Step 2: Load doctor data from CSV
+    create_database()
+    load_doctors_data()
     print("Database initialized successfully.")
